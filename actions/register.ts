cb1293f5f859b839bd9add057a62dbe536db74ac
ihebbenothmen <98585612+ihebbenothmen@users.noreply.github.com>
2024-11-08 02:22:50 +0100

    Database-&-Prisma-setup

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,8 @@
 "use server";
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
-
+import { db } from "@/lib/db";
+import bcrypt from "bcrypt";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -10,6 +11,29 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     if (!validatedFields.success) {
         return { error: "Invalid fields!" };
     }
+    const { email, password, name } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    const existingUser = await db.user.findUnique({
+        where: {
+            email,
+        }
+    });
 
-    return { success: "Email sent!" };
+    if (existingUser) {
+        return { error: "Email already in use!" }
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        },
+    });
+
+    // TODO: Send verification token email 
+
+
+    return { success: "User created!" };
 };
