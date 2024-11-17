adfe34092e8bbc4b33675ffbd265a1fcc72a8a1f
ihebbenothmen <98585612+ihebbenothmen@users.noreply.github.com>
2024-11-17 17:13:05 +0100

    error-card-message

--- auth.ts
+++ auth.ts
@@ -11,7 +11,7 @@ declare module "@auth/core"; {
         user: {
             role: string;
 
-        } & DefaultSession ["user"]
+        } & DefaultSession["user"]
     }
 }
 
@@ -20,27 +20,39 @@ export const { handlers: { GET, POST },
     signIn,
     signOut,
 
-}
-    = NextAuth({
-        callbacks: {
-            async session({ session, token }) {
-                if (token.sub && session.user) {
-                    session.user.id = token.sub;
-                }
-                if (token.role && session.user) {
-                    session.user.role = token.role as UserRole  ;
-                }
-                return session
-            },
-            async jwt({ token }) {
-                if (!token.sub) return token;
-                const existingUser = await getUserById(token.sub);
-                if (!existingUser) return token;
-                token.role = existingUser.role;
-                return token
+} = NextAuth({
+    pages:{
+        signIn: "/auth/login",
+        error: "/auth/error",
+    } ,
+    events: {
+        async linkAccount({ user }) {
+            await db.user.update({
+                where: { id: user.id },
+                data: { emailVerified: new Date()}
+
+            })
+        }
+    },
+    callbacks: {
+        async session({ session, token }) {
+            if (token.sub && session.user) {
+                session.user.id = token.sub;
             }
+            if (token.role && session.user) {
+                session.user.role = token.role as UserRole;
+            }
+            return session
         },
-        adapter: PrismaAdapter(db),
-        session: { strategy: "jwt" },
-        ...authConfig,
-    });
+        async jwt({ token }) {
+            if (!token.sub) return token;
+            const existingUser = await getUserById(token.sub);
+            if (!existingUser) return token;
+            token.role = existingUser.role;
+            return token
+        }
+    },
+    adapter: PrismaAdapter(db),
+    session: { strategy: "jwt" },
+    ...authConfig,
+});
